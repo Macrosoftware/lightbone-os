ce989a39c8832679debbcb8d3aa2d671a43f2dbe
Pawel3-linux <pawelek.rz@o2.pl>
2016-02-23 20:36:29 +0100

    File is building.

--- window.hpp
+++ window.hpp
@@ -1,3 +1,19 @@
 class Window
 {
+    private:
+        short x;
+        short y;
+        short cx;
+        short cy;
+        char * caption;
+        Edit * edit_cmps;
+        Button * button_cmps;
+        CheckBox * checkbox_cmps;
+        RadioBox * radiobox_cmps;
+        List * list_cmps;
+        Window * parentwnd;
+        char cornersch[4];
+        char captionlinechar;
+        char linesupchar;
+    public:
 };
