3a03b3026b0078fc9eca4f5a364ce17c6f79a4eb
Pawel3-linux <pawelek.rz@o2.pl>
2016-02-23 20:48:33 +0100

    File is building.

--- window.hpp
+++ window.hpp
@@ -15,5 +15,11 @@ class Window
         char cornersch[4];
         char captionlinechar;
         char linesupchar;
+
     public:
+        void operator()(short _x, short _y, short _cx, short _cy, char * _caption, Window * _parentwnd, char * _cornersch, char _captionlinechar, char _linesupchar)
+        {
+            if (x < 30 && x > 0)
+                x = _x;
+        }
 };
